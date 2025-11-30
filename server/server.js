55af79b2e0926cc1d8383a2ed82a7a9a0f50a1ef
EdwardJamik <edward.babetskiy@gmail.com>
2025-11-30 19:56:18 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-require("dotenv").config();
+require("dotenv").config({ quiet: true });
 const bodyParser = require('body-parser');
 const path = require("path");
 const fs = require("fs");
